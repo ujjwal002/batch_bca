a452890dfb293fc71935dcef74ae3f75d21698b6
Ujjwal Kumar <ujjwalsinghrajput2000@gmail.com>
2025-09-13 19:28:00 +0530

    added day 23

---
+++ day22/index.cpp
@@ -0,0 +1,16 @@
+class Student{
+    student(){
+
+    }
+
+    come()
+    play()
+    walk()
+}
+
+student s1 = new Student()
+
+s1.play
+
+s1.play
+s1.walk
---
+++ day23/task.md
@@ -0,0 +1,3 @@
+# make a list of differnt differnt type of selector in js
+
+# what are the different differnt type of event in js
